4580e0a597fcc082590580ba2f7a27cc5586c033
davidbarone <davidbarone@live.com>
2021-02-20 20:59:12 +1100

    Added pragmaFrag to babel config.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
 import Didact from "@dbarone/didact"
+const { render, useEffect } = Didact
 
-function Counter(props) {
-  Didact.useEffect(() => { alert("Example of useEffect") });
+const Counter = () => {
+  useEffect(() => { alert("in useEffect!") });
+
   return (
-    <div>
-      <div>{props.value}</div>
-    </div>
+    <div>Hello World!</div>
   )
 }
 
-function MyTable() {
-  const [state, setState] = Didact.useState(1)
-  return (
-    <div>
-      <h1 onClick={() => setState(c => c + 1)}>Click Me!</h1>      
-      <Counter message="Counter A" value={state} />
-      <Counter message = "Counter B" value={state} />
-      <Counter message = "Counter C" value={state} />
-    </div>
-  )
-}
-
-const element = <MyTable />
-const container = document.getElementById("root")
-Didact.render(element, container)
+const element = <Counter />;
+const container = document.getElementById("root");
+render(element, container);
